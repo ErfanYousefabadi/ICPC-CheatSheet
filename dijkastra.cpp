99fa8e2b07e9d54127fe805e739eb2ee3d4834c6
ErfanYousefabadi <erfan.yoosefabadi@gmail.com>
2025-12-25 00:21:19 +0330

    added mark to dijkstra

--- dijkastra.cpp
+++ dijkastra.cpp
@@ -2,16 +2,15 @@ priority_queue <pll, vector<pll>, greater<pll>> pq;
 
 memset(dis, 63, sizeof dis);
 dis[1] = 0;
-q.push({0, 1});
-
+pq.push({0, 1});
 while (pq.size()) {
-    auto k = pq.top();
-    int v = k.S;
+    int v = pq.top().S;
     pq.pop();
-    for (auto u: adj[v]) {
-        if (dis[v] + u.S < dis[u.F]) {
-            dis[u.F] = dis[v] + u.S;
-            pq.push({dis[u.F], u.F});
-        }
+    if (mark[v]) continue;
+    mark[v] = true;
+    for (auto u: adj[v])
+    if (dis[u.F] > dis[v] + u.S) {
+        dis[u.F] = dis[v] + u.S;
+        pq.push({dis[u.F], u.F});
     }
 }
