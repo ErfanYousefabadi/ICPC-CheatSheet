7e6728f2f02b97178d80a476ffceedbdc47b9751
ErfanYousefabadi <erfan.yoosefabadi@gmail.com>
2025-12-24 20:03:55 +0330

    suffix array + geo

--- SuffixArray.cpp
+++ SuffixArray.cpp
@@ -1,25 +1,27 @@
-ll n , p[maxn] , h[lg][maxn] , sa[maxn] , lcp[maxn] , mark[maxn] , ps[maxn] , l[maxn] , r[maxn] , cp , ans ; 
-string s , t ; 
-stack<int> st ; 
-bool cmp(int i , int j){
-	if(h[cp-1][i] < h[cp-1][j]) return true; 
-	if(h[cp-1][i] > h[cp-1][j]) return false ; 
-	if(i + (1 << (cp-1)) > n + 1 || j + (1 << (cp-1)) > n + 1) return i < j ;  
-	return h[cp-1][i + (1 << (cp-1))] < h[cp-1][j + (1 << (cp-1))] ; 
+int n, cp, p[N];
+string s;
+int h[LG][N];
+ 
+bool cmp(int a , int b){
+  if (h[cp - 1][a] < h[cp - 1][b]) return true;
+  else if (h[cp - 1][a] > h[cp - 1][b]) return false; 
+  if (a + (1 << (cp - 1)) > n + 1 || b + (1 << (cp - 1)) > n + 1) return a > b; 
+  return h[cp - 1][a + (1 << (cp - 1))] < h[cp - 1][b + (1 << (cp - 1))];
 }
-void SA(){
-	for(int i = 1 ; i <= n ; i ++ ) p[i] = i , h[0][i] = s[i]; 
-	for(cp = 1 ; cp < lg ; cp++ ){
-		sort(p +1 , p + n + 1 , cmp) ; 
-		h[cp][p[1]] =1 ; 
-		for(int i = 2 ; i <= n ; i ++ ) h[cp][p[i]] = h[cp][p[i-1]] + cmp(p[i-1] , p[i]) ; 
-	}
+cin >> s; 
+s = '#' + s;  
+n = s.size() - 1; 
+for (int i = 1; i <= n; i++) {
+    h[0][i] = s[i];
+    p[i] = i;
 }
-void Lcp(){
-	for(int i = 1 , k = 0 ; i <= n ; i ++ ){
-		if(sa[i] == n) continue ; 
-		for(int j = p[sa[i] + 1] ; k <= n && s[i+k] == s[j+k] ; ) k++ ; 
-		lcp[sa[i]] = k ; 
-		if(k) k -- ; 
-	}
+for (cp = 1; cp < LG; cp++) {
+    sort(p + 1, p + n + 1, cmp);
+    h[cp][p[1]] = 1; 
+    for (int i = 2; i <= n; i++) {
+        h[cp][p[i]] = h[cp][p[i - 1]] + cmp(p[i - 1] , p[i]);  
+    }
 }
+p[0] = n + 1;
+for (int i = 0; i <= n; i++)
+    cout << p[i] << ' ';
